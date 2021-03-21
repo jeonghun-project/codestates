24447124a19d6a4cfb06ca71e9e5596fdbbf37cd
jeonghun-project <dsjoh111@gmail.com>
2021-03-21 22:50:13 +0900

    update ignore

--- posts/.vuepress/theme/layouts/GlobalLayout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="GlobalLayout">
-    <component :is="$page.path ? 'Layout' : 'NotFound'" />
-    <footer class="siteFooter">
-      &copy; Devlog from jeong
-    </footer>
-  </div>
-</template>
