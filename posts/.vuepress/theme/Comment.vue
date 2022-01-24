91623f75bbe0b6dc38721bbb7c025bd241b4a4c8
jeonghun-project <dsjoh111@gmail.com>
2022-01-24 12:41:57 +0900

    vuepress update

--- posts/.vuepress/theme/Comment.vue
+++ posts/.vuepress/theme/Comment.vue
@@ -13,7 +13,7 @@ export default {
     
     utterances.setAttribute('issue-term', 'pathname'); // pathname|url|title|og:title 중 택 1
     utterances.setAttribute('theme','github-light'); // theme 설정
-    utterances.setAttribute('repo',`junilhwang/blog-comment`); // 사용할 repository
+    utterances.setAttribute('repo',`jeonghun-project/blog-comment`); // 사용할 repository
 
     // script tag 삽입
     this.$refs.comment.appendChild(utterances);
